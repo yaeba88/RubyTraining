978b84c5adad9c42f0474bb31d58e6add793b216
yaeba88 <kkomeda510@gmail.com>
2014-04-20 13:23:36 +0900

    add comment for too-bad code

--- app/string_expansion.rb
+++ app/string_expansion.rb
@@ -15,24 +15,26 @@ class String
     return result.join
   end
 
+  # えげつないので正規表現を用いて要リファクタリング
   def to_snake
     result = []
     tmp_str = ''
     capitalize_sequence_count = 0
 
     self.split('').each do |word|
-      if word.match(/[A-Z]/)
-        if tmp_str.length > 0 && capitalize_sequence_count === 0
+      if word.match(/[A-Z]/)  # 大文字
+        if tmp_str.length > 0 && capitalize_sequence_count === 0  # 小文字->大文字の流れなら一単語として抽出
           result.push tmp_str
           tmp_str = ''
         end
         tmp_str = tmp_str + word.downcase
         capitalize_sequence_count += 1
-      else
-        if capitalize_sequence_count > 1
+
+      else                    # 小文字
+        if capitalize_sequence_count > 1  # 複数大文字->小文字の流れなら、直前の文字までを一単語として抽出
           result.push tmp_str.slice(0, tmp_str.length-1)
           tmp_str = tmp_str.slice(tmp_str.length-1, 1) + word.downcase
-        else 
+        else
           tmp_str = tmp_str + word.downcase
         end
         capitalize_sequence_count = 0
